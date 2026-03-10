a3f71c90c41e308893f9f02211b9cc7525be8170
Строков Артур Александрович <AAStrokov@alfabank.ru>
2026-03-10 18:46:40 +0300

    delete validation from tool

--- src/main/java/com/gmail/arthurstrokov/translatemcpserver/tool/TranslateTool.java
+++ src/main/java/com/gmail/arthurstrokov/translatemcpserver/tool/TranslateTool.java
@@ -1,8 +1,6 @@
 package com.gmail.arthurstrokov.translatemcpserver.tool;
 
 import com.gmail.arthurstrokov.translatemcpserver.service.OllamaChatClientService;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.context.annotation.Lazy;
@@ -18,7 +16,7 @@ public class TranslateTool {
     }
 
     @Tool(name = "translate", description = "Translate text from English to Russian")
-    public String translate(@ToolParam(description = "Text to translate (max 5000 characters)") @NotBlank @Size(max = 5000) String text) {
+    public String translate(@ToolParam(description = "Text to translate") String text) {
         return ollamaChatClientService.ask(text);
     }
 }
