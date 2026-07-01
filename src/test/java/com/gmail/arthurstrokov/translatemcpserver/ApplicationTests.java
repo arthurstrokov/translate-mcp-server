bf28d62233a07214ba55f13ea3503bad840862bc
Строков Артур Александрович <AAStrokov@alfabank.ru>
2026-07-01 21:48:21 +0300

    add aot test

--- src/test/java/com/gmail/arthurstrokov/translatemcpserver/ApplicationTests.java
+++ src/test/java/com/gmail/arthurstrokov/translatemcpserver/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.gmail.arthurstrokov.translatemcpserver;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTests.class);
+
     @Test
     void contextLoads() {
         // This test verifies that the application context can be loaded.
+        String property = System.getProperty("spring.aot.enabled");
+        log.info(property);
     }
 }
